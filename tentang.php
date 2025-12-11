1a0237b34f5c3a4b59014789d60b71c2406e0566
Ananda Faris Ghazi Ramadhan <164128337+SheraFaris@users.noreply.github.com>
2025-12-11 15:22:12 +0800

    Create tentang.php

---
+++ tentang.php
@@ -0,0 +1,16 @@
+<?php include 'header.php'; ?>
+
+<div class="card">
+    <div class="card-body">
+        <h3 class="text-center mb-3">Tentang</h3>
+        <p>
+            Laundry Crafty merupakan sebuah sistem informasi sederhana berbasis web.
+            Laundry Crafty juga merupakan contoh aplikasi web CRUD sederhana
+            dengan PHP dan MySQL. Laundry Crafty digunakan untuk melakukan
+            pendataan terhadap laundry masuk dan laundry keluar.
+            Laundry Crafty dibangun menggunakan PHP, Bootstrap, dan MySQL.
+        </p>
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
